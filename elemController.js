fd0ad5799bdf7877ef47321d11e2b84ace380855
Celia Angles <celiaangles22@gmail.com>
2023-10-31 16:25:01 +0100

    solved coins

--- elemController.js
+++ elemController.js
@@ -1,8 +1,8 @@
 import Element from "./Element.js";
 
 export default class ElemController {
-  ELEMENT_INTERVAL_MIN = 100;
-  ELEMENT_INTERVAL_MAX = 3000;
+  ELEMENT_INTERVAL_MIN = 500;
+  ELEMENT_INTERVAL_MAX = 2000;
 
   nextElementInterval = null;
   elem = [];
@@ -13,7 +13,6 @@ export default class ElemController {
     this.elemImages = elemImages;
     this.scaleRatio = scaleRatio;
     this.speed = speed;
-
     this.setNextElementTime();
   }
 
@@ -34,7 +33,7 @@ export default class ElemController {
     const index = this.getRandomNumber(0, this.elemImages.length - 1);
     const elementImages = this.elemImages[index];
     const x = this.canvas.width * 1.5;
-    const y = this.canvas.height - elementImages.height; //editable
+    const y = this.canvas.height - elementImages.height;
     const element = new Element(
       this.ctx,
       x,
@@ -44,23 +43,23 @@ export default class ElemController {
       elementImages.image
     );
     this.elem.push(element);
+    console.log(this.elem);
   }
 
   update(gameSpeed, frameTimeDelta) {
     if (this.nextElementInterval <= 0) {
+      this.createElement();
       this.setNextElementTime();
     }
     this.nextElementInterval -= frameTimeDelta;
 
-    console.log(this.elem.length);
-
-    this.elem.forEach((element) => {
-      element.update(this.speed, gameSpeed, frameTimeDelta, this.scaleRatio);
+    this.elem.forEach((e) => {
+      e.update(this.speed, gameSpeed, frameTimeDelta, this.scaleRatio);
     });
-    this.elem = this.elem.filter((element) => element.x > -element.width);
+    this.elem = this.elem.filter((e) => e.x > -e.width);
   }
 
   draw() {
-    this.elem.forEach((element) => element.draw());
+    this.elem.forEach((e) => e.draw());
   }
 }
